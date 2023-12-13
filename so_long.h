029f6460371fe76a183c7f6470e0d2de92b1c675
43skhool <43skhool@gmail.com>
2023-12-13 16:30:34 +0100

    make remaking and libft inserting

--- so_long.h
+++ so_long.h
@@ -35,14 +35,13 @@ typedef struct s_map_validation_response
 }	map_validation_response;
 
 map_validation_response	*get_map(char *file_name, char **map_matrix);
-void	dealloc_matrix(char **matrix);
-char **duplicate_char_matrix(char	**matrix);
-size_t	count_matrix_row(char    **matrix);
-int	ft_strcmp(char *s1, char *s2);
 
-// size_t	ft_strlen(const char	*str);// TO DO da levare e rimpiazzare con la funzione dentro libft
-// size_t	ft_strlcpy(char *dest, const char *src, size_t size); // TO DO da levare e rimpiazzare con la funzione dentro libft
-// void	*ft_calloc(size_t nitems, size_t size);// TO DO da levare e rimpiazzare con la funzione dentro libft
-// char	**ft_split(char const *s, char c);// TO DO da levare e rimpiazzare con la funzione dentro libft
+void	dealloc_matrix(char **matrix);
+
+char **duplicate_char_matrix(char	**matrix);
+
+size_t	count_matrix_row(char    **matrix);
+
+int	ft_strcmp(char *s1, char *s2);
 
 #endif
