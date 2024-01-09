75ad45f69a554da8a5b3aec028562a0dfdb2af51
Marco Ceccarelli <maceccar@c2r2p12.42firenze.it>
2024-01-09 16:09:12 +0100

    norminetting

--- map_validation/map_parsing.c
+++ map_validation/map_parsing.c
@@ -1,45 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_parsing.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maceccar <maceccar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/09 15:16:44 by maceccar          #+#    #+#             */
+/*   Updated: 2024/01/09 15:16:44 by maceccar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../so_long.h"
 
-t_game *validate_map(t_game *game);
-static bool validate_file_name(char *file_name);
-static char **read_map(char *file_name, t_game *game);
-static bool get_map_size(char *file_name, t_game *game);
+t_game		*validate_map(t_game *game);
+static bool	validate_file_name(char *file_name);
+static void	read_map(char *file_name, t_game *game);
+static bool	get_map_size(char *file_name, t_game *game);
+static void	allocate_map(t_game *game);
 
-t_game *get_map(char *file_name)
+t_game	*get_map(char *file_name)
 {
-	t_game *game;
+	t_game	*game;
+
 	game = malloc(sizeof(t_game));
 	if (!game)
 		return (NULL);
 	game->is_map_valid = false;
-	game->exit_position = NULL;
-	game->player_position = NULL;
-	game->map = NULL;
-
 	if (validate_file_name(file_name) == false)
-		return (game->reason = "Error, wrong file extension", game);
-
+	{
+		game->reason = "Error, wrong file extension";
+		return (game);
+	}
 	if (get_map_size(file_name, game) == false)
-		return (game->reason = "Error, map isn't a rectangle or it is too small", game);
-
-	game->map = read_map(file_name, game);
-
+	{
+		game->reason = "Error, map isn't a rectangle or it is too small";
+		return (game);
+	}
+	read_map(file_name, game);
 	if (!game->map)
 		return (game->reason = "Error during file read", game);
-
 	game = validate_map(game);
 	if (!game)
 		return (game->reason = "Error during map validation", game);
-
 	return (game);
 }
 
-static bool validate_file_name(char *file_name)
+// check if file is a '.ber'
+static bool	validate_file_name(char *file_name)
 {
-	char **file_name_splitted;
-	size_t row_count;
-	int lenght;
+	char	**file_name_splitted;
+	size_t	row_count;
+	int		lenght;
 
 	file_name_splitted = ft_split(file_name, '.');
 	row_count = count_matrix_row(file_name_splitted);
@@ -52,59 +63,56 @@ static bool validate_file_name(char *file_name)
 	dealloc_matrix(file_name_splitted);
 	return (false);
 }
-// read the map from file and check if it's a rectangle
-// abbastanza sicuro che in caso di una lettura non completa del file (del buffer statico),
-// la parte rimanente della riga valga come memory leak alla fine del programma.
 
-// read the map from file and check if it's a rectangle and the file is .ber file
-static char **read_map(char *file_name, t_game *game)
+//map reading from file and check if is a rectangle
+static void	read_map(char *file_name, t_game *game)
 {
-	int	i;
-	int	j;
-	int	fd;
-	char buffer[1];
-	char **map;
+	int		i;
+	int		j;
+	int		fd;
+	char	buffer[1];
 
-	//buffer = ft_calloc(size->columns_number + 1, sizeof(char));
-	i = 0;
+	allocate_map(game);
 	j = 0;
-	map = ft_calloc(game->number_of_rows + 1, sizeof(char *));
-	while(i < game->number_of_rows)
-	{
-		map[i] = ft_calloc(game->number_of_columns + 1, sizeof(char));
-		i++;
-	}
-
 	i = 0;
 	fd = open(file_name, O_RDONLY);
-	while(read(fd, buffer, 1) > 0)
+	while (read(fd, buffer, 1) > 0)
 	{
 		if (*buffer == '\n')
 		{
 			i++;
 			j = 0;
 		}
-		else if (*buffer != '\r')//carriage return
+		else if (*buffer != '\r')
 		{
-			map[i][j] = *buffer;
+			game->map[i][j] = *buffer;
 			j++;
 		}
 	}
 	close(fd);
-	return (map);
 }
 
-// read the file lookinf for the dimensions of the map to allow the allocation and returno false
-// if the map isn't a rectangle
-static bool get_map_size(char *file_name, t_game *game)
+static void	allocate_map(t_game *game)
+{
+	int	i;
+
+	i = 0;
+	game->map = ft_calloc(game->number_of_rows + 1, sizeof(char *));
+	while (i < game->number_of_rows)
+	{
+		game->map[i] = ft_calloc(game->number_of_columns + 1, sizeof(char));
+		i++;
+	}
+}
+
+// check if is a rectangle and save the size
+static bool	get_map_size(char *file_name, t_game *game)
 {
 	int			fd;
 	char		buffer[1];
-	//int			line_length;
-	int			column_number;
+	int			n;
 
-	column_number = 0;
-	//line_length = 0;
+	n = 0;
 	fd = open(file_name, O_RDONLY);
 	game->number_of_rows = 1;
 	game->number_of_columns = 0;
@@ -113,17 +121,16 @@ static bool get_map_size(char *file_name, t_game *game)
 		if (*buffer == '\n')
 		{
 			game->number_of_rows++;
-			if (game->number_of_columns != column_number && game->number_of_columns != 0)//1° line
+			if (game->number_of_columns != n && game->number_of_columns != 0)
 				return (false);
 			else
-				game->number_of_columns = column_number;
-			column_number = 0;
+				game->number_of_columns = n;
+			n = 0;
 		}
-		else if (*buffer != '\r')//carriage return
-			column_number++;
+		else if (*buffer != '\r')
+			n++;
 	}
-	close(fd);
 	if (*buffer == '\n' || *buffer == '\r' || game->number_of_columns <= 1)
-		return (false);
-	return (true);
+		return (close(fd), false);
+	return (close(fd), true);
 }
