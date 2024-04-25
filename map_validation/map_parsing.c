00b40972e92d869c47855db885d16a5c51a0a7ce
43skhool <43skhool@gmail.com>
2024-04-25 02:51:44 +0200

    Normi on Gnl

--- map_validation/map_parsing.c
+++ map_validation/map_parsing.c
@@ -108,49 +108,6 @@ static void	allocate_map(t_game *game)
 	}
 }
 
-//check if is a rectangle and save the size
-// static t_bool	get_map_size(char *file_name, t_game *game)
-// {
-// 	int			fd;
-// 	char		buffer[1];
-// 	int			n;
-
-// 	n = 0;
-// 	fd = open(file_name, O_RDONLY);
-// 	game->number_of_rows = 1;
-// 	game->number_of_columns = 0;
-// 	while (read(fd, buffer, sizeof(buffer)) > 0)
-// 	{
-// 		if (*buffer == '\n')
-// 		{
-// 			game->number_of_rows++;
-// 			if (game->number_of_columns != n && game->number_of_columns != 0)
-// 				return (false);
-// 			else
-// 				game->number_of_columns = n;
-// 			n = 0;
-// 		}
-// 		else if (*buffer != '\r')
-// 			n++;
-// 	}
-// 	if (buffer[0] == 4)
-// 		return (close(fd), false);
-// 	if (*buffer == '\n' || *buffer == '\r' || game->number_of_columns <= 1)
-// 		return (close(fd), false);
-// 	return (close(fd), true);
-// }
-
-//Apro il file
-//Leggo una riga
-//Finche viene letto qualcosa
-//	check lunghezza
-//		OK -> avanti
-//		NO -> clean e return false
-//
-//	assegnazione numero di colonne
-//	libero buffer attuale
-//	leggo la prossima riga
-//	aumento il numero di righe
 static t_bool	get_map_size(char *file_name, t_game *game)
 {
 	char	*buffer;
@@ -177,38 +134,7 @@ static t_bool	get_map_size(char *file_name, t_game *game)
 
 	get_next_line(fd, true);
 	close(fd);
+	if (game->number_of_rows == 0)
+		result = false;
 	return (result);
 }
-
-//Check for empty first line
-// static t_bool	get_map_size(char *file_name, t_game *game)
-// {
-// 	char	*line;
-// 	int		fd;
-// 	t_bool	error;
-
-// 	fd = open(file_name, O_RDONLY);
-// 	line = get_next_line(fd, false);
-// 	if (line != "")
-// 	{
-// 		game->number_of_columns = ft_strlen(line);
-// 		while (line != NULL)
-// 		{
-// 			line = get_next_line(fd, false);
-// 			// if (ft_strlen(line) != game->number_of_columns && game->number_of_columns != 0)
-// 			// {
-// 			// 	error = true;
-// 			// 	break;
-// 			// }
-// 			game->number_of_columns = ft_strlen(line);
-// 			game->number_of_rows++;
-// 		}
-// 	}
-// 	else
-// 		error = true;
-
-// 	get_next_line(fd, true);
-// 	fd = close(fd);
-// 	printf("%i\n", game->number_of_rows);
-// 	return (error);
-// }
